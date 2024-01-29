07743aea05218758bd1678069d6cde84c7158df5
Ignacio98 <dani.manchu1998@gmail.com>
2024-01-28 23:03:54 -0500

    Fixing bugs in admod_helper.dart

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2761501950649437~4384901280";
+      return "ca-app-pub-2761501950649437/7147684040";
     } else if (Platform.isIOS) {
       return "";
     } else {
